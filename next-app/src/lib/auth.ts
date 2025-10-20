253ecaec78d2eb444799a4b6d49855c8d8f08232
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-10-20 05:28:39 +0300

    feat: migrated the db to support better auth

--- next-app/src/lib/auth.ts
+++ next-app/src/lib/auth.ts
@@ -1,7 +1,16 @@
+import { PrismaClient } from "@/generated/prisma";
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { anonymous } from "better-auth/plugins";
 
 const auth = betterAuth({
-  //...
+  database: prismaAdapter(PrismaClient, {
+    provider: "sqlite", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [anonymous()],
 });
 
 export { auth };
